42de5c5ebb473c9fe8439c8cf51ee0b1020d88bd
Emily Wen <emilywen2000@gmail.com>
2019-04-07 17:30:35 -0700

    added datadetail text

--- src/components/dataDetail/DataDetail.jsx
+++ src/components/dataDetail/DataDetail.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 //import { Link } from 'react-router-dom';
 
 class DataDetail extends React.Component {
-
-	render() {
-		return(
-			<div className="left">
-				<div>Dataset: </div>
-				<div className="small">
-					Yoba
-				</div>
-			</div>
-		)
-	}
+    render() {
+	return(
+	    <div className="left">
+	    	 <div id = "name"> add dataset name </div>
+		 <p> Source: add source </p>
+		 <p> Last Updated: add date </p>
+		 <div> add description </div>
+	    </div>
+	    	    
+	    <div className = "top-right">
+	    	 <Link to="/">View Data </Link>
+		 <Link to="/">Visualize</Link>
+		 <Link to="/">Export</Link>
+		 <Link to="/">API Call </Link>
+	    </div>
+	    
+	    <div className = "data"> add table of data here </div>
+	)
+    }
 }
 
 export default DataDetail;
