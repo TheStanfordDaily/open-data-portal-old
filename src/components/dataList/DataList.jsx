e4480c017b60df3a2e57baa4406d6ea7225a4c0f
Alex Goodman <alexgood@stanford.edu>
2019-04-07 18:50:56 -0700

    finished wiring front end to back end to S3 bucket

--- src/components/dataList/DataList.jsx
+++ src/components/dataList/DataList.jsx
@@ -1,51 +1,71 @@
 import React from 'react';
 import axios from 'axios';
-import { Link, Route } from 'react-router-dom';
-import DataDetail from '../dataDetail/DataDetail.jsx';
-import SideBar from '../sideBar/SideBar.jsx';
+import DataItem from '../dataItem/DataItem.jsx';
+//import { Link, Route } from 'react-router-dom';
+//import DataDetail from '../dataDetail/DataDetail.jsx';
+//import SideBar from '../sideBar/SideBar.jsx';
 
-let array = [
-  {
-    name: "Rendering with React",
-    source: "-source-",
-    description: "-description-"
-  },
-  {
-    name: "Components",
-    source: "-source-",
-    description: "-description-"
-  },
-  {
-    name: "Props v. State",
-    source: "-source-",
-    description: "-description-"
+class DataList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data_list: undefined,
+    };
   }
-];
 
-const DataList = ({ match }) => (
-  <div>
-  <SideBar />
-    <h2>Datasets</h2>
+  render() {
+    return (
+        <div>
+          <ul>
+            {this.state.data_list && this.state.data_list.map((i) => <li key={i.name}><DataItem obj={i} /></li>)}
+          </ul>
+        </div>
+    );
+  }
 
-    <ul>
-      {array.map((element) =>
-            <li key={element.name}> 
-              <Link to={`${match.url}/{element.name}`}>
-                {element.name}
-              </Link><br></br>
-              <label>Source: {element.source}</label><br></br>
-              <label>Description: {element.description}</label>
-            </li>
-      )}
-    </ul>
+  componentDidMount() {
+    axios.get('http://localhost:9000/datasets/list').then(
+        (success) => {
+          this.setState({
+            data_list: success.data,
+          });
+        }, (failure) => {
+          console.log(failure);
+        }
+    );
+  }
+}
 
-    <Route path={`${match.url}/:dataId`} component={DataDetail} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a dataset.</h3>}
-    />
-  </div>
-);
+// const DataList = ({ match }) => (
+//   <div>
+//   <SideBar />
+//     <h2>Datasets</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>
+//           Rendering with React
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>
+//           Components
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>
+//           Props v. State
+//         </Link>
+//       </li>
+//     </ul>
+//
+//     <Route path={`${match.url}/:dataId`} component={DataDetail} />
+//     <Route
+//       exact
+//       path={match.url}
+//       render={() => <h3>Please select a dataset.</h3>}
+//     />
+//   </div>
+// );
 
 export default DataList;
