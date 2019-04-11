731f0e828da9f8214fd00a4e36048d09e159a4c1
Arjun Ramani <arjunramani3@gmail.com>
2019-04-11 11:07:07 +0100

    DataDetail can now download data

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import About from './components/about/About.jsx';
 import Home from './components/homePage/HomePage.jsx';
 import DataList from './components/dataList/DataList.jsx';
 import TopBar from './components/topBar/TopBar.jsx';
+import DataDetail from './components/dataDetail/DataDetail.jsx';
 
 const BasicExample = () => (
   <Router>
      <div>
-      <link rel="stylesheet" 
+      <link rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossorigin="anonymous"
@@ -25,6 +26,7 @@ const BasicExample = () => (
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/data-list" component={DataList} />
+      <Route path="/datasets/:dataName" component={DataDetail} />
     </div>
     </div>
   </Router>
