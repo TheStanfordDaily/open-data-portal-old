dd60ce5027cb8b434bceed05123a593f5f09ec30
catzhang <test@test.com>
2019-04-13 10:26:51 -0700

    Updated index and topBar to correctly direct to submit page

---
+++ credentials.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+    accessKey: "AKIAY3D4NIKYB5ZUT4R5",
+    accessKeySecret: "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",
+    bucket: "open-data-portal",
+};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import About from './components/about/About.jsx';
 import Home from './components/homePage/HomePage.jsx';
 import DataList from './components/dataList/DataList.jsx';
 import TopBar from './components/topBar/TopBar.jsx';
+import Submit from './components/submit/Submit.jsx';
 
 const BasicExample = () => (
   <Router>
@@ -18,6 +19,7 @@ const BasicExample = () => (
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/data-list" component={DataList} />
+      <Route path="/submit" component={Submit} />
     </div>
   </Router>
 );
