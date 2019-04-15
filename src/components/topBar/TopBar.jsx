2d98d0a5d0df3736fd238de6b8454cb035b09c50
Emily Wen <emilywen2000@gmail.com>
2019-04-15 16:33:34 -0700

    Make navbar fixed to top

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class TopBar extends React.Component {
     render() {
 	return(
-	  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+	  <nav className="navbar fixed-top navbar-light navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#">Logo </a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -42,11 +42,11 @@ class TopBar extends React.Component {
         <a className="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <form className="form-inline my-2 my-lg-0">
+  </div>
+  <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-  </div>
 </nav>
     	)
     }
