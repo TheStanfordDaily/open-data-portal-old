169805957f89d3d98f8aae401cfe68882508f8d4
Emily Wen <emilywen2000@gmail.com>
2019-05-14 12:50:50 -0700

    change sidebar to a form

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,92 +1,62 @@
 import React from 'react';
+import Form from 'react-jsonschema-form'
 //import { Link } from 'react-router-dom';
 
 class SideBar extends React.Component {
-	constructor(props) {
-    super(props);
-
-    this.state = {
-      categories: [],
-    };
-
-	this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-	  
-  }
-
-
-
-	render() {
+	render(){
+		let schema ={
+			"title": "Filter By:",
+			"type": "object",
+			"required": [
+				
+			],
+			"properties": {
+				"category": {
+					"type": "array",
+					"title": "Category:",
+					"items": {
+						"type": "string",
+						"enum": [
+							" Finance",
+							" Academics",
+							" Student Life",
+							" R&DE"
+						]
+					},
+					"uniqueItems": true
+				},
+				"data_type": {
+					"type": "array",
+					"title": "Data Type:",
+					"items": {
+						"type": "string",
+						"enum": [
+							" Numerical",
+							" Geospatial",
+							" Categorical",
+							" Temporal"
+						]
+					},
+					"uniqueItems": true
+				}
+			}
+		}
+		let uiSchema = {
+			"category": {
+				"ui:widget": "checkboxes"
+			},
+			"data_type": {
+				"ui:widget": "checkboxes"
+			}
+		}
 		return(
 			<div className = "container">
-			<form>
-				<h5>Filter By</h5>
-				<div id="filter" className="all">
-					<div>
-						<input type="checkbox" name="finances" value="finances" />
-						<label for="finances">Finances</label>
-					</div>
-					<div>
-						<input type="checkbox" name="academics" value="academics" />
-						<label for="academics">Academics</label>
-					</div>
-					<div>
-						<input type="checkbox" name="studentdorms" value="studentdorms" />
-						<label for="studentdorms">Student Life</label>
-					</div>
-					<div>
-						<input type="checkbox" name="r&de" value="r&de" />
-						<label for="r&de">R&DE</label>
-					</div>
-				</div>
-
-				<h5>Include Data Types</h5>
-				<div id="included" className="all">
-					<div>
-						<input type="checkbox" name="numerical" value="numerical" />
-						<label for="numerical">Numerical</label>
-					</div>
-					<div>
-						<input type="checkbox" name="categorical" value="categorical" />
-						<label for="categorical">Categorical</label>
-					</div>
-					<div>
-						<input type="checkbox" name="geospatial" value="geospatial" />
-						<label for="geospatial">Geospatial</label>
-					</div>
-					<div>
-						<input type="checkbox" name="temporal" value="temporal" />
-						<label for="temporal">Temporal</label>
-					</div>
-					<div>
-						Start
-						<select>
-						  <option value="2015">2015</option>
-						  <option value="2016">2016</option>
-						  <option value="2017">2017</option>
-						  <option value="2018">2018</option>
-						</select>
-					</div>
-					<div>
-						End
-						<select>
-						  <option value="2015">2015</option>
-						  <option value="2016">2016</option>
-						  <option value="2017">2017</option>
-						  <option value="2018">2018</option>
-						</select>
-					</div>
-				</div>
-
-				<div>
-			  		<button type="submit" className="btn btn-primary" onClick={() =>
-						this.handleSubmit()}>Submit!</button>
-			  	</div>
-			</form>
+			 <p>Search for the dataset you want! </p>
+				<Form schema={schema} uiSchema={uiSchema} onSubmit={e => console.log(e.formData)} >
+					<button>Search</button>
+				</Form>
 			</div>
-		)
+		);
 	}
 }
 
