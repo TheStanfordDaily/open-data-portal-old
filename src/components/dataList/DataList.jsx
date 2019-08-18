cc0cda909c1cab9e89df378139e3dc5633a6b6bb
Emily Wen <emilywen2000@gmail.com>
2019-08-18 15:02:20 -0400

    update

--- src/components/dataList/DataList.jsx
+++ src/components/dataList/DataList.jsx
@@ -12,7 +12,7 @@ class DataList extends React.Component {
     this.state = {
       all_datasets: undefined,
       datasets_to_display: undefined,
-      filters: "",
+      filters: '',
     };
 
     this.updateFilters = this.updateFilters.bind(this);
@@ -32,56 +32,56 @@ class DataList extends React.Component {
     );
   }
 
-  updateFilters(newFilters) {
-    console.log("new data", newFilters);
-    console.log("pre filters", this.state.filters);
+  updateFilters(newData) {
+    console.log("newdata", newData);
     let all_datasets = this.state.all_datasets;
-
-    // console.log("category", newData.category)
-    let final = newFilters === "" ? 
-      all_datasets :
-      all_datasets.filter((post) => {
-        // console.log("tags", post.tags);
-        return newFilters.includes(post.tags);
-      }
-    );
-  
+    let final = newData === undefined ? all_datasets :
+    all_datasets.filter((post) => {
+      return newData.includes(post.tags);
+    });
+    console.log(final);
     this.setState({
-      filters: newFilters,
-      datasets_to_display: final
+      filters: newData,
+      datasets_to_display: final,
+    }, () => {
+      console.log("update", newData, final, this.state);
+      debugger;
     })
-    console.log("state in datalist", this.state);
+    
+    debugger;
   }
 
   render() {
+    console.log("props", this.state.filters);
+    debugger;
     return (
         <div className = "container">
           <h2> Data Catalog</h2>
           <div className = "row">
-            <div className = "col-3"> <SideBar filters={this.state.filters} updateFilters={this.updateFilters}/> </div>
+          <div className = "col-3"> <SideBar filters={this.state.filters} updateFilters={this.updateFilters}/> </div>
 
-            <div className = "col-9">
-              {this.state.all_datasets && this.state.datasets_to_display.map((post) =>
-                <div>
-                  <div className = "card card-body">
-                    <h4 className = "card-title">
-                      <Link to={{
-                        pathname: '/datasets/' + post.name,
-                        state: {
-                          data: post,
-                        }
-                      }}>{post.display_name}</Link>
-                    </h4>
-                    <p className = "card-text"> {post.description} </p>
-                    <small>
-                      <i><a href = {post.source_url}> Source </a> </i>
-                    </small>
-                    <small> Posted on {post.create_date}  </small>
-                  </div>
-                  <br></br>
-                </div>
-              )}
-            </div>
+          <div className = "col-9">
+
+          {this.state.all_datasets && this.state.datasets_to_display.map((post) =>
+          <div>
+          <div className = "card card-body">
+                <h4 className = "card-title">
+                  <Link to={{
+                    pathname: '/datasets/' + post.name,
+                    state: {
+                      data: post,
+                    }
+                  }}>{post.display_name}</Link>
+                </h4>
+                <p className = "card-text"> {post.description} </p>
+                <small><i><a href = {post.source_url}> Source </a> </i></small>
+                <small> Posted on {post.create_date}  </small>
+
+          </div>
+          <br></br>
+          </div>
+          )}
+          </div>
           </div>
         </div>
     );
--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,54 +1,77 @@
 import React from 'react';
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-jsonschema-form'
+import Form from 'react-jsonschema-form'
 //import { Link } from 'react-router-dom';
 
 class SideBar extends React.Component {
 	constructor(props) {
 		super(props);
+		console.log("sidebarprops", this.props);
 		this.state = {
-      filters: ""
+			filters: this.props.filters
 		};
+		this.handleSubmit = this.handleSubmit.bind(this);
 		console.log("this.state", this.state)
 	}
 
-	updatecheckboxes(name) {
-		console.log(name);
+	handleSubmit(data) {
+		console.log("data", data);
+		debugger;
+	  this.props.updateFilters(data);
 	}
+	handleCheckboxChange(data) {
+		console.log("name", data);
+		if (this.state.filters.includes(data)) {
+			this.setState({
+				filters: this.state.filters.replace(data+',','')
+			}, () => {
+				console.log("removed", this.state.filters);
+			})
+		} else {
+			this.setState({
+				filters: this.state.filters + data + ","
+			}, () => {
+				console.log("added", this.state.filters);
+			})
+		}
+		debugger;
+	}
+
 	render() {
-		console.log("props", this.props);
 		return (
 			<div className = "container">
 				<p>Search for the dataset you want! </p>
-				<form className="form-inline">
-				<div className="form-check">
-					<input className="form-check-input" type="checkbox" value={this.state.filters.includes("Finances")} id="Finances"/>
-					<label className="form-check-label" for="Finances">
-						Finances
-					</label>
-				</div>
-				<div className="form-check">
-					<input className="form-check-input" type="checkbox" value={this.state.filters.includes("Academics")} id="Academics"/>
-					<label className="form-check-label" for="Academics">
-						Academics
-					</label>
-				</div>
-				<div className="form-check">	
-					<input className="form-check-input" type="checkbox" value={this.state.filters.includes("Students")} id="Students"/>
-					<label className="form-check-label" for="Students">
-						Students
-					</label>
-				</div>
-				<div className="form-check">	
-				<input className="form-check-input" type="checkbox" value={this.state.filters.includes("R&DE")} id="R&DE"/>
-					<label className="form-check-label" for="R&DE">
-						R&DE
-					</label>
-				</div>
-				<button type="submit" className="btn btn-primary mb-2" onSubmit={this.props.updateFilters(this.state.filters)}>
-					Search
-				</button>
+				<form className="form-inline" onSubmit={e => this.handleSubmit(this.state.filters)}>
+					<div className="form-check">
+						<input className="form-check-input" type="checkbox" id="Finances" 
+							checked={this.state.filters.includes("Finances")} onChange={e => this.handleCheckboxChange("Finances")}/>
+							<label className="form-check-label" for="Finances">
+								Finances
+							</label>
+					</div>
+					<div className="form-check">
+						<input className="form-check-input" type="checkbox" id="Academics"
+							checked={this.state.filters.includes("Academics")} onChange={e => this.handleCheckboxChange("Academics")}/>
+							<label className="form-check-label" for="Academics">
+								Academics
+							</label>
+					</div>
+					<div className="form-check">	
+						<input className="form-check-input" type="checkbox" id="Students"
+							checked={this.state.filters.includes("Students")} onChange={e => this.handleCheckboxChange("Students")}/>
+							<label className="form-check-label" for="Students">
+								Students
+							</label>
+					</div>
+					<div className="form-check">	
+					<input className="form-check-input" type="checkbox" id="R&DE"
+						checked={this.state.filters.includes("R&DE")} onChange={e => this.handleCheckboxChange("R&DE")}/>
+						<label className="form-check-label" for="R&DE">
+							R&DE
+						</label>
+					</div>
+					<button type="submit" className="btn btn-primary mb-2">
+						Search
+					</button>
 				</form>
 			</div>
 		)
