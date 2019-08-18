a8715c41799d920a3662f87b7bb89ad94a80c39c
Emily Wen <emilywen2000@gmail.com>
2019-08-18 13:41:52 -0400

    indentation fix

--- src/components/dataList/DataList.jsx
+++ src/components/dataList/DataList.jsx
@@ -58,31 +58,30 @@ class DataList extends React.Component {
         <div className = "container">
           <h2> Data Catalog</h2>
           <div className = "row">
-          <div className = "col-3"> <SideBar filters={this.state.filters} updateFilters={this.updateFilters}/> </div>
+            <div className = "col-3"> <SideBar filters={this.state.filters} updateFilters={this.updateFilters}/> </div>
 
-          <div className = "col-9">
-
-          {this.state.all_datasets && this.state.datasets_to_display.map((post) =>
-            <div>
-              <div className = "card card-body">
-                <h4 className = "card-title">
-                  <Link to={{
-                    pathname: '/datasets/' + post.name,
-                    state: {
-                      data: post,
-                    }
-                  }}>{post.display_name}</Link>
-                </h4>
-                <p className = "card-text"> {post.description} </p>
-                <small>
-                  <i><a href = {post.source_url}> Source </a> </i>
-                </small>
-                <small> Posted on {post.create_date}  </small>
-              </div>
-              <br></br>
+            <div className = "col-9">
+              {this.state.all_datasets && this.state.datasets_to_display.map((post) =>
+                <div>
+                  <div className = "card card-body">
+                    <h4 className = "card-title">
+                      <Link to={{
+                        pathname: '/datasets/' + post.name,
+                        state: {
+                          data: post,
+                        }
+                      }}>{post.display_name}</Link>
+                    </h4>
+                    <p className = "card-text"> {post.description} </p>
+                    <small>
+                      <i><a href = {post.source_url}> Source </a> </i>
+                    </small>
+                    <small> Posted on {post.create_date}  </small>
+                  </div>
+                  <br></br>
+                </div>
+              )}
             </div>
-          )}
-          </div>
           </div>
         </div>
     );
