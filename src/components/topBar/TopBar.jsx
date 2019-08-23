6f566f58def568cb070ef16ee69966cac04ef366
Nik Marda <nmarda@stanford.edu>
2019-08-23 16:34:17 -0500

    fixed nav bar

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 import logo from './logo.jpg';
 
 class TopBar extends React.Component {
-  constructor(props) {
-		super(props);
-		this.state = {
-			search: '',
-		};
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-  
+    render() {
+  return(
+    <nav className="navbar static-top navbar-light navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand navbar-caption text-black display-1" href="/"><img src={logo} alt="logo" style={{height: '5.1rem'}} />   SODP</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
@@ -30,7 +29,7 @@ class TopBar extends React.Component {
           Affiliated Sites
         </a>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="https://stanforddaily.com/" target="_blank">The Stanford Daily</a>
+          <a className="dropdown-item" href="https://stanforddaily.com/" target="_blank"> Stanford Daily</a>
         </div>
       </li>
     </ul>
